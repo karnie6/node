a50a0f56930b26dbeaaf72edecd7b20c681fc40d
Karthik Senthil <karthik@trywildcard.com>
2016-01-06 13:29:54 -0500

    more additions to the chatroom

--- ChatCat_Begin/app.js
+++ ChatCat_Begin/app.js
@@ -5,7 +5,10 @@ var express = require('express');
 	session = require('express-session'),
 	config = require('./config/config.js'),
 	ConnectMongo = require('connect-mongo')(session),
-	mongoose = require('mongoose').connect(config.dbURL);
+	mongoose = require('mongoose').connect(config.dbURL),
+	passport = require('passport'),
+	FacebookStrategy = require('passport-facebook').Strategy,
+	rooms = [];
 
 app.set('views', path.join(__dirname, 'views'));
 app.engine('html', require('hogan-express'));
@@ -44,10 +47,22 @@ var Karthik = new Person({
 	fullName:'Karthik Senthil'
 }); */
 
+app.use(passport.initialize());
+app.use(passport.session());
 
-require('./routes/routes.js')(express, app);
+require('./routes/routes.js')(express, app, passport, config, rooms);
+require('./auth/passportAuth.js')(passport, FacebookStrategy, config, mongoose);
 
-app.listen(3000, function() {
+/*app.listen(3000, function() {
 	console.log('ChatCat running on port 3000');
 	console.log('Mode: ', env);
+}) */
+
+app.set('port', process.env.PORT || 3000);
+var server = require('http').createServer(app);
+var io = require('socket.io').listen(server);
+require('./socket/socket.js')(io, rooms);
+
+server.listen(app.get('port'), function() {
+	console.log("ChatCat on Port " + app.get('port'));
 })
--- ChatCat_Begin/routes/routes.js
+++ ChatCat_Begin/routes/routes.js
@@ -1,4 +1,4 @@
-module.exports = function(express, app){
+module.exports = function(express, app, passport, rooms){
 	var router = express.Router();
 
 	router.get('/', function(req, res, next){
@@ -7,10 +7,59 @@ module.exports = function(express, app){
 		});
 	});
 
-	router.get('/chatrooms', function(req, res, next) {
+	function securePages(req, res, next) {
+		if(req.isAuthenticated()) {
+			next();
+		} else {
+			res.redirect('/');
+		}
+	};
+
+	router.get('/chatrooms', securePages, function(req, res, next) {
 		res.render('chatrooms', {
-			title: 'Chatrooms'
+			title: 'Chatrooms',
+			user: req.user,
+			config: config
 		});
+	});
+
+	router.get('/room/:id', securePages, function(req, res, next) {
+		
+		var room_name = getRoomTitle(req.params.id);
+		res.render('room', {
+			room_name: room_name,
+			user: req.user,
+			config: config,
+			room_number:req.params.id,
+			title: 'Room'
+		});
+
+	});
+
+	function getRoomTitle(room_id) {
+		var n = 0;
+		while (n < rooms.length) {
+			if (rooms[n].room_number === room_id) {
+				return rooms[n].room_name;
+				break;
+			} else {
+				n++;
+				continue;
+			}
+ 		}
+	}
+
+
+
+	router.get('/auth/facebook', passport.authenticate('facebook'));
+	router.get('/auth/facebook/callback', passport.authenticate('facebook', {
+		sucessRedirect:'/chatrooms',
+		failureRedirect:'/'
+	}));
+
+	router.get('/logout', function(req, res, next) {
+		req.logout();
+		res.redirect('/');
 	});
 
 	app.use('/', router);
